98ba2d356788407072eb7614865db112da3dc8d4
Jose Lazo <jjlazo79@gmail.com>
2023-04-13 13:22:07 +0200

    Update rank-math.php

--- rank-math.php
+++ rank-math.php
@@ -4,6 +4,7 @@
  * Filter to change the page title.
  * 
  * @param string $title
+ * @link https://rankmath.com/kb/filters-hooks-api-developer/
  */
 add_filter( 'rank_math/frontend/title', function( $title ) {
     if (strlen($title) > 60) $title = str_replace(' | Institut Vila-Rovira', '', $title);
